6d0c80a73d71561a01403486004cb0845aa5d5e1
孔大夫 <kxb51812@gmail.com>
2020-06-17 23:04:15 +0800

    add

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link
+} from 'react-router-dom';
+import routes from './router';
+import { RouteWithSubRoutes } from './assets/common';
+import { RouteInterface } from './assets/interface';
+import './App.less';
+import { Button } from 'antd';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="App">
+      {/* <Button type="primary">Button</Button>
+        <header className="App-header">
+          <ul>
+            <li><Link to="/home">Home 首页</Link></li>
+            <li><Link to="/about">About 关于</Link></li>
+            <li><Link to="/link">Link 联系</Link></li>
+          </ul>
+        </header> */}
+
+        <Switch>
+          {routes.map((route: RouteInterface, i: number) => {
+            return RouteWithSubRoutes(route, i)
+          })}
+        </Switch>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
