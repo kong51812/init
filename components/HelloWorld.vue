62987dd929b20f1b2e8e4d05d7724bd4939d2db1
孔祥斌 <kongxb@yuntongxun.com>
2020-06-21 18:33:59 +0800

    init

---
+++ components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="BG">
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        count: 0
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+
+  }
+</script>
+<style lang="scss">
+  .BG {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.7);
+    color: white;
+
+  }
+</style>
--- src/plugins/element.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import { Button,Row, Col, } from 'element-ui'
-
-Vue.use(Button)
-Vue.use(Row)
-Vue.use(Col)
